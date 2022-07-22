bb204c812fca483c60c675f74ced3f935059261c
chanwit-y <chanwit.yimneam@hotmail.com>
2022-07-22 19:25:45 +0700

    +

--- wm-workspace/packages/components-bk/jest.config.ts
+++ wm-workspace/packages/components-bk/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
-  displayName: 'components',
+  displayName: 'components-bk',
   preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/components',
+  coverageDirectory: '../../coverage/packages/components-bk',
 };
